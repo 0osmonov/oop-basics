4e2c6fe3a72eb1bae0239f8f2a92ae9e82a75d74
osmonov <osmonbekovnurtilek176@gmail.com>
2026-05-09 22:42:58 +0300

    обновил

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,32 +1,24 @@
 class Hero:
-    # Атрибуты класса через конструктор __init__
     def __init__(self, name, level, health, strength):
         self.name = name
         self.level = level
         self.health = health
         self.strength = strength
 
-    # 1. Метод приветствия
     def greet(self):
         print(f"Привет, я {self.name}, мой уровень {self.level}")
 
-    # 2. Метод атаки
     def attack(self):
         print(f"{self.name} наносит удар!")
-        self.strength -= 1  # Уменьшаем силу на 1
-
-    # 3. Метод отдыха
+        self.strength -= 1  
     def rest(self):
         print(f"{self.name} отдыхает…")
-        self.health += 1  # Увеличиваем здоровье на 1
+        self.health += 1  
 
-# --- Проверка задания ---
 
-# Создаем 2 объекта
 hero1 = Hero("Арагорн", 10, 100, 20)
 hero2 = Hero("Леголас", 12, 80, 15)
 
-# Проверяем первого героя
 hero1.greet()
 print(f"Статы до: Здоровье {hero1.health}, Сила {hero1.strength}")
 hero1.attack()
@@ -35,7 +27,6 @@ print(f"Статы после: Здоровье {hero1.health}, Сила {hero1.
 
 print("-" * 20)
 
-# Проверяем второго героя
 hero2.greet()
 hero2.attack()
 hero2.rest()
